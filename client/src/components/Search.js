cdf28ad213b29d1d2e306cf33e65bc0dc9cb5d78
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-10-11 09:29:17 +0300

    post request created from search component to send the search input value to backend

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import Axios from "axios";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,21 +12,43 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   header: {
-    "& > *": {
-      margin: theme.spacing(3),
-      // width: '40ch',
-    },
     paddingLeft: 20,
   },
 }));
 
 const Search = () => {
   const classes = useStyles();
+  const [info, setInfo] = useState([]);
+
+  const handleChange = async (e) => {
+    e.preventDefault();
+    if (e.target.value.length > 0) {
+      await Axios.post("http://localhost:3005/search", {
+        searchValue: e.target.value,
+      })
+        .then(
+          (response) => {
+            setInfo(response.data);
+          }
+        )
+        .then((error) => console.log(error));
+    }
+  };
 
   return (
     <div>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Search" variant="outlined" />
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onChange={(e) => e.preventDefault}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          onChange={handleChange}
+        />
       </form>
       <Typography variant="h4" color="inherit" className={classes.header}>
         Result:
